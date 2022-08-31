41ecef4b2a2ea09c0e31bb682bce12e66d2d746d
liladhar4 <patil.liladha4@gmail.com>
2022-08-31 22:44:41 +0530

    26 August Assignment

---
+++ 26AugustAssignment.js
@@ -0,0 +1,23 @@
+(function(){
+    function newArryPrevNext( arr1) {
+        
+        if (n <= 1) 
+            return; 
+        let pre_elem = arr1[0]; 
+        arr1[0] = arr1[0] * arr1[1]; 
+        for (let i=1; i<n-1; i++) { 
+            let cur_elem = arr1[i]; 
+            arr1[i] = pre_elem * arr1[i+1]; 
+            pre_elem = cur_elem; 
+        } 
+        arr1[n-1] = pre_elem * arr1[n-1]; 
+        return arr1
+} 
+let arr1=[1,2,3,4,5,6];
+let n = arr1.length
+document.write("original Array ==> "+arr1);
+document.write("<br>");
+document.write("Previous Number and Next Number Multipication ==> "+newArryPrevNext(arr1));
+
+
+})();
